6feb79e116214ebd73343b21f514d91a21582d20
heartnetkung <heartnetkung@gmail.com>
2015-12-02 06:28:10 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var path = require('path');
 var root = __dirname.replace(/([\\\/]node_modules[\\\/][^\\\/]+)/g, '');
-var requirePath = path.resolve(root, '/bin/WebChimera.js.node');
+var requirePath = path.join(root, '/bin/WebChimera.js.node');
 
 try {
 	module.exports = require(requirePath);
